fe12ef4d907aa1ed3ae8e67cd0237f7d6ebe6517
Tomer Eliel <tomereliel0@gmail.com>
2021-04-01 02:00:34 +0300

    fixing block locating

--- easydb/src/canvas.js
+++ easydb/src/canvas.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import './canvas.css';
-import Block from './block'
+import React from "react";
+import "./canvas.css";
+import Block from "./block";
 
-
-export default class Canvas extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { x:null, y:null,blocks:[] };
-      }
-    state = {
-
-    };
-
-    addBlock(params) {
-        this.setState({ x: params.screenX, y: params.screenY }); 
-        //this.state.x =params.screenX;
-        //this.state.y =params.screenY;
-        this.state.blocks.push(<Block x={this.state.x} y={this.state.y}/>)
-        
-    };
-    render() {
-      return (
-        <div className="canvas" onClick={this.addBlock.bind(this)}>
-            <h1>x: {this.state.x} y: {this.state.y}</h1>
-            <div>
-                {this.state.blocks}
-            </div>
-        </div>
-      );
-    }
+export default class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: null, y: null, blocks: [] };
   }
+  state = {};
+
+  addBlock(params) {
+    //this.setState({ x: params.screenX, y: params.screenY });
+    //this.state.x =params.screenX;
+    //this.state.y =params.screenY;
+    this.state.blocks.push(<Block x={this.state.x} y={this.state.y} />);
+    const tempBlocks = this.state.blocks;
+    this.setState({ blocks: tempBlocks });
+  }
+  _onMouseMove(e) {
+    this.setState({
+      x: e.nativeEvent.offsetX - 15,
+      y: e.nativeEvent.offsetY - 15,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div
+          className="canvas"
+          onMouseMove={this._onMouseMove.bind(this)}
+          onClick={this.addBlock.bind(this)}
+        >
+          <h1>
+            x: {this.state.x} y: {this.state.y}
+          </h1>
+        </div>
+        <div>{this.state.blocks}</div>
+      </div>
+    );
+  }
+}
